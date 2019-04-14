ac213686108eeb61cc8ab55469b1a6d64cd7cb46
Chang Liu <lcqwop@gmail.com>
2019-04-14 15:53:51 -0400

    task 5 and 6

--- hw4.py
+++ hw4.py
@@ -2,7 +2,7 @@ import logging
 import re
 import sys
 from bs4 import BeautifulSoup
-from queue import Queue
+from queue import Queue, PriorityQueue
 from urllib import parse, request
 
 logging.basicConfig(level=logging.DEBUG, filename='output.log', filemode='w')
@@ -24,7 +24,14 @@ def parse_links(root, html):
 
 def parse_links_sorted(root, html):
     # TODO: implement
-    return []
+    pq = PriorityQueue()
+    for link in parse_links(root, html):
+        pq.put((-len(link[1]), link))
+    result = []
+    while not pq.empty():
+        result.append(pq.get()[1])
+        print(pq.get()[1])
+    return result
 
 
 def get_links(url):
@@ -47,7 +54,7 @@ def urls_same_host(url1, url2):
 
 def content_match(content_type, wanted_list):
     for wanted_content in wanted_list:
-        if content_type in wanted_content:
+        if wanted_content in content_type :
             return True
     return False
 
@@ -58,15 +65,16 @@ def crawl(root, wanted_content=[], within_domain=True):
     '''
     # TODO: implement
 
-    queue = Queue()
-    queue.put(root)
+    pq = PriorityQueue()
+    pq.put((0, root))
 
     visited = []
     extracted = []
     url_set = set()
 
-    while not queue.empty():
-        url = queue.get()
+
+    while not pq.empty():
+        url = pq.get()[1]
         try:
             req = request.urlopen(url, timeout=5)
             html = req.read()
@@ -74,16 +82,19 @@ def crawl(root, wanted_content=[], within_domain=True):
             visited.append(url)
             visitlog.debug(url)
 
-            if req.headers['Content-Type'] and content_match(req.headers['Content-Type'], wanted_content):
+            if len(visited) > 100:
+                break
+
+            if len(wanted_content) == 0 or (req.headers['Content-Type'] and content_match(req.headers['Content-Type'], wanted_content)):
                 for ex in extract_information(url, html):
                     extracted.append(ex)
                     extractlog.debug(ex)
 
             for link, title in parse_links(url, html):
                 if (link not in url_set) and (urls_same_host(root, link) or not within_domain):
-                    print(len(visited), queue.qsize(), link)
+                    print(len(visited), pq.qsize(), link)
                     url_set.add(link)
-                    queue.put(link)
+                    pq.put((-len(title), link))
 
         except Exception as e:
             print(e, url)
@@ -97,8 +108,12 @@ def extract_information(address, html):
 
     # TODO: implement
     results = []
-    for match in re.findall('\d\d\d-\d\d\d-\d\d\d\d', str(html)):
+    for match in re.findall('(?:\([0-9]{3}\)-?|[0-9]{3}[-.])[0-9]{3}[-.][0-9]{4}', str(html)):
         results.append((address, 'PHONE', match))
+    for match in re.findall('\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}', str(html)):
+        results.append((address, 'EMAIL', match))
+    for match in re.findall('(?:[A-Z][a-z]*\s)*(?:[A-Z][a-z]*)+,\s*(?:AK|Alaska|AL|Alabama|AR|Arkansas|AZ|Arizona|CA|California|CO|Colorado|CT|Connecticut|DC|Washington\sDC|Washington\D\.C\.|DE|Delaware|FL|Florida|GA|Georgia|GU|Guam|HI|Hawaii|IA|Iowa|ID|Idaho|IL|Illinois|IN|Indiana|KS|Kansas|KY|Kentucky|LA|Louisiana|MA|Massachusetts|MD|Maryland|ME|Maine|MI|Michigan|MN|Minnesota|MO|Missouri|MS|Mississippi|MT|Montana|NC|North\sCarolina|ND|North\sDakota|NE|New\sEngland|NH|New\sHampshire|NJ|New\sJersey|NM|New\sMexico|NV|Nevada|NY|New\sYork|OH|Ohio|OK|Oklahoma|OR|Oregon|PA|Pennsylvania|RI|Rhode\sIsland|SC|South\sCarolina|SD|South\sDakota|TN|Tennessee|TX|Texas|UT|Utah|VA|Virginia|VI|Virgin\sIslands|VT|Vermont|WA|Washington|WI|Wisconsin|WV|West\sVirginia|WY|Wyoming)\s+\d{5}(?:-\d{4})?', str(html)):
+        results.append((address, 'ADDRESS', match))
     return results
 
 
@@ -117,7 +132,7 @@ def main():
     nonlocal_links = get_nonlocal_links(site)
     writelines('nonlocal.txt', nonlocal_links)
 
-    visited, extracted = crawl(site, wanted_content='html')
+    visited, extracted = crawl(site)
     writelines('visited.txt', visited)
     writelines('extracted.txt', extracted)
 
